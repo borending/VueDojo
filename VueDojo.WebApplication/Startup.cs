d08f4ba5ec4ac88f428ce3983398972eb0c2c1aa
GasparDing <kai21413@gmail.com>
2019-12-09 17:33:10 +0800

    basic rendering

--- VueDojo.WebApplication/Startup.cs
+++ VueDojo.WebApplication/Startup.cs
@@ -53,7 +53,7 @@ namespace VueDojo.WebApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Vue}/{action=Index}/{id?}");
             });
         }
     }
